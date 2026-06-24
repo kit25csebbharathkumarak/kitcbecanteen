32b4179900a0b21e61738259d04d98e5039fbe62
Bharath Kumar <bharath@example.com>
2026-06-24 20:52:59 +0530

    Force IPv4 for DNS to fix ENETUNREACH

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('dns').setDefaultResultOrder('ipv4first');
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
